c0db5387851374c5298ee5462190953f839e0ced
SantiPorres <sporres09@gmail.com>
2023-10-09 21:59:14 -0500

    Initial commit

---
+++ OcelotGatewayExample/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using OcelotGatewayExample.Aggregators;
+using OcelotGatewayExample.Handlers;
+using System;
+using System.Text;
+
+namespace OcelotGatewayExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("helloworldhelloworld")),
+                        ClockSkew = new TimeSpan(0)
+                    };
+                });
+
+            services.AddOcelot()
+                .AddDelegatingHandler<RemoveEncodingDelegatingHandler>(true)
+                .AddSingletonDefinedAggregator<UsersPostsAggregator>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseOcelot().Wait();
+        }
+    }
+}
